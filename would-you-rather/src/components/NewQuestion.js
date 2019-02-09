7b57f9d5b41ab22bd2a6eb6b075cc31bb67b612f
Ben Flores <beflore@microsoft.com>
2019-02-09 15:00:59 -0800

    Create NewQuestion basic component

---
+++ would-you-rather/src/components/NewQuestion.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NewQuestion extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="center">NewQuestion</h3>
+      </div>
+    );
+  }
+}
