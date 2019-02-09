26036e201cc5d16f6394856daa991f83bbb7472c
Ben Flores <beflore@microsoft.com>
2019-02-09 14:38:29 -0800

    Update App

--- would-you-rather/src/components/App.js
+++ would-you-rather/src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import "../App.css";
+import Nav from "./Nav";
+import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h3>Welcome!</h3>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          <header className="App-header">
+            <h3>Welcome!</h3>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
