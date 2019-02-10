c2739ddb4f8a3afd1cca14e383775e8531344728
Ben Flores <beflore@microsoft.com>
2019-02-09 18:43:48 -0800

    Pull questions from mapStateToProps

--- would-you-rather/src/components/Home.js
+++ would-you-rather/src/components/Home.js
@@ -10,15 +10,20 @@ class Home extends Component {
         {this.props.users.map(user => {
           return <p key={user}>{user}</p>;
         })}
+
+        {this.props.questions.map((question, index) => {
+          return <p key={index}>{question}</p>;
+        })}
         <Login />
       </div>
     );
   }
 }
 
-function mapStateToProps({ users }) {
+function mapStateToProps({ users, questions }) {
   return {
-    users: Object.keys(users)
+    users: Object.keys(users),
+    questions: Object.keys(questions)
   };
 }
 
