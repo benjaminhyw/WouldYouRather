8eaff564d5f53f4535939f803b3dd77e7d85bfd2
Ben Flores <beflore@microsoft.com>
2019-02-09 20:07:32 -0800

    Remove dead comments

--- would-you-rather/src/utils/helpers.js
+++ would-you-rather/src/utils/helpers.js
@@ -1,9 +1,6 @@
 export function formatQuestion(question, author, authedUser) {
   const { id, timestamp, optionOne, optionTwo } = question;
   const { name, avatarURL } = author;
-  //   const { optionOneVotes, optionOneText } = optionOne;
-  //   const { optionTwoVotes, optionTwoText } = optionTwo;
-  // const { name, avatarURL, answers, questions } = author;
 
   return {
     name,
