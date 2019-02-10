ccd8b303e685ff5c294fe6ea005ee26946590688
Ben Flores <beflore@microsoft.com>
2019-02-10 11:54:06 -0800

    Connext Leader board to redux store, add basic logic inside User component, import and use in Leaderboar

--- would-you-rather/src/components/Leaderboard.js
+++ would-you-rather/src/components/Leaderboard.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import User from "./User";
 
-export default class Leaderboard extends Component {
+class Leaderboard extends Component {
   render() {
     return (
       <div>
         <h3 className="center">Leaderboard</h3>
+        <User />
       </div>
     );
   }
 }
+
+function mapStateToProps({ users }) {
+  return {
+    userIds: Object.keys(users)
+  };
+}
+
+export default connect(mapStateToProps)(Leaderboard);
