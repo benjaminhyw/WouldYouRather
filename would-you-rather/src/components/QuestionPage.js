2d5e21b2d502bc0fe655e9751eeae1d7ac0da704
Ben Flores <beflore@microsoft.com>
2019-02-09 20:56:49 -0800

    Update Question Page component

--- would-you-rather/src/components/QuestionPage.js
+++ would-you-rather/src/components/QuestionPage.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Question from "./Question";
+import { Link } from "react-router-dom";
 
 class QuestionPage extends Component {
   render() {
-    return <Question id={this.props.id} />;
+    return <Question id={this.props.id} questionDisplay={QuestionPoll} />;
   }
+}
+
+function QuestionPoll(question) {
+  return (
+    <div>
+      <div>Would you rather...</div>
+      <div>...{question.optionOne.text}...</div>
+      <div>OR</div>
+      <div>...{question.optionTwo.text}...</div>
+      <button className="btn">Submit</button>
+    </div>
+  );
 }
 
 function mapStateToProps({ authedUser, questions, users }, props) {
