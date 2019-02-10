79c2b6b5bd61908d90d6f28881a483d0566456f2
Ben Flores <beflore@microsoft.com>
2019-02-09 16:00:24 -0800

    import and use new Login component, need to implement logic

--- would-you-rather/src/components/Home.js
+++ would-you-rather/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Login from "./Login";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <h3 className="center">Home</h3>
+        <Login />
       </div>
     );
   }
