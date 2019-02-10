ccc3362440eb6e504db5b1b6443a5b57072673c8
Ben Flores <beflore@microsoft.com>
2019-02-09 21:01:40 -0800

    Update NewQuestion component, added basic form

--- would-you-rather/src/components/NewQuestion.js
+++ would-you-rather/src/components/NewQuestion.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 export default class NewQuestion extends Component {
   render() {
     return (
-      <div>
-        <h3 className="center">NewQuestion</h3>
+      <div className="center">
+        <h3 className="center">Create New Question</h3>
+        <div>Would you rather ...</div>
+        <form>
+          <input />
+          <div>OR</div>
+          <input />
+          <br />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
