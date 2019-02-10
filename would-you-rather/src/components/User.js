c9d93f3335beddde6ace361561b1416436aa200b
Ben Flores <beflore@microsoft.com>
2019-02-10 12:20:32 -0800

    Update User component

--- would-you-rather/src/components/User.js
+++ would-you-rather/src/components/User.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatUser } from "../utils/helpers";
+import { Link, withRouter } from "react-router-dom";
 
-export default class User extends Component {
+class User extends Component {
   render() {
-    return <div>USER COMPONENT</div>;
+    const { user } = this.props;
+    if (!user) {
+      return <p>This user doesn't exist</p>;
+    }
+    const { id, name, answers, questions, avatar } = user;
+
+    return <div>{name}</div>;
   }
 }
+
+function mapStateToProps({ authedUser, users, questions }, { id }) {
+  const user = users[id];
+
+  return {
+    authedUser,
+    user: user ? formatUser(user, authedUser) : null
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(User));
