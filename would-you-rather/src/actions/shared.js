a71535507c3661c386fa4f1bf21e209425c09121
Ben Flores <beflore@microsoft.com>
2019-02-09 18:33:31 -0800

    dispatch receiveQuestions action inside getInitialData

--- would-you-rather/src/actions/shared.js
+++ would-you-rather/src/actions/shared.js
@@ -1,6 +1,7 @@
 import { getInitialData } from "../utils/api";
 import { setAuthedUser } from "../actions/authedUser";
 import { receiveUsers } from "../actions/users";
+import { receiveQuestions } from "../actions/questions";
 
 const AUTHED_ID = "tylermcginnis";
 
@@ -8,6 +9,7 @@ export function handleInitialData() {
   return dispatch => {
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
       dispatch(setAuthedUser(AUTHED_ID));
     });
   };
