a07b3aedcffa5a900ef4f6a2f40ebbf5fe270075
Ben Flores <beflore@microsoft.com>
2019-02-10 13:13:12 -0800

    Update Nav logic, display logged in user & logout link if someone is logged in

--- would-you-rather/src/components/Nav.js
+++ would-you-rather/src/components/Nav.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 
-export default function Nav() {
-  return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <NavLink to="/" exact activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/newQuestion" exact activeClassName="active">
-            Create New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/leaderboard" exact activeClassName="active">
-            Leaderboard
-          </NavLink>
-        </li>
-      </ul>
-      <ul>
-        <li>Hello, USER</li>
-        <li>
-          <NavLink to="/" exact activeClassName="active">
-            Logout
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="nav">
+        <ul>
+          <li>
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/newQuestion" exact activeClassName="active">
+              Create New Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/leaderboard" exact activeClassName="active">
+              Leaderboard
+            </NavLink>
+          </li>
+        </ul>
+        <ul>
+          {this.props.authedUser && (
+            <div className="flexbox">
+              <li>Hello, {this.props.authedUser}</li>
+              <li>
+                <NavLink to="/" exact activeClassName="active">
+                  Logout
+                </NavLink>
+              </li>
+            </div>
+          )}
+        </ul>
+      </nav>
+    );
+  }
 }
+
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser
+  };
+}
+
+export default connect(mapStateToProps)(Nav);
