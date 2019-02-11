9d7b33f2a0764355040bcfa94bb3a9eabfab6a90
Ben Flores <beflore@microsoft.com>
2019-02-10 17:20:58 -0800

    Update Login logic

--- would-you-rather/src/components/Login.js
+++ would-you-rather/src/components/Login.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { Dropdown } from "office-ui-fabric-react";
 
-export default class Login extends Component {
+class LoginForm extends Component {
   render() {
+    const { handleSubmit } = this.props; // might change to locally defined
+
     return (
-      <div>
-        <h3 className="center">Login</h3>
+      <div className="center">
+        <h3>Login</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Field name="userId" component={Dropdown} type="text" options />
+          </div>
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
 }
+
+LoginForm = reduxForm({
+  form: "login"
+})(LoginForm);
+
+export default LoginForm;
