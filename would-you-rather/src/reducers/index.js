5a75b0bf74842857f2efb0449596cccf1835fe7e
Ben Flores <beflore@microsoft.com>
2019-02-10 16:54:43 -0800

    Add redux-form reducer to combineReducers

--- would-you-rather/src/reducers/index.js
+++ would-you-rather/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authedUser from "./authedUser";
 import users from "./users";
 import questions from "./questions";
+import { reducer as reduxFormReducer } from "redux-form";
 
 export default combineReducers({
   authedUser,
   users,
-  questions
+  questions,
+  reduxFormReducer
 });
